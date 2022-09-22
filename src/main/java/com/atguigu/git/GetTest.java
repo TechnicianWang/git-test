4d69fb94e5aebf0c3332c1fca96c61b4505bd18a
WangZhen <wangzhen_0195@163.com>
2022-09-22 10:43:36 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GetTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: GetTest <br/>
+ * Description: <br/>
+ * date: 2022/9/22 10:25<br/>
+ *
+ * @author 王震<br />
+ * @since JDK 1.8
+ */
+public class GetTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
