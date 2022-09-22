ff703621ab6d8dd4dc5eddf86871bd74975da7ef
WangZhen <wangzhen_0195@163.com>
2022-09-22 11:30:48 +0800

    push commit

--- src/main/java/com/atguigu/git/GetTest.java
+++ src/main/java/com/atguigu/git/GetTest.java
@@ -16,5 +16,6 @@ public class GetTest {
         System.out.println("Hello Git4!");
         System.out.println("master test!");
         System.out.println("hit-fix test");
+        System.out.println("push test");
     }
 }
