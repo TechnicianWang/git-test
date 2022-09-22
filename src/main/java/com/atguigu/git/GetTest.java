7287cf5d9f0ea4844ef2cfed08e4e04add3c3a11
WangZhen <wangzhen_0195@163.com>
2022-09-22 11:00:02 +0800

    third commit

--- src/main/java/com/atguigu/git/GetTest.java
+++ src/main/java/com/atguigu/git/GetTest.java
@@ -11,6 +11,7 @@ package com.atguigu.git;
 public class GetTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
+        System.out.println("Hello Git3!");
     }
 }
